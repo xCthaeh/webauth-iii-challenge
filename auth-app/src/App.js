3d8f27c9e1e8f75999617726f81d3d4bc45a485b
Alyssa Jane <xcthaeh@gmail.com>
2019-06-12 17:49:37 -0400

    created react app shell

---
+++ auth-app/src/App.js
@@ -0,0 +1,10 @@
+// import React, { Component } from "react";
+// import axios from "axios";
+// import Register from "./components/register";
+// import Login from "./components/login";
+// import { withRouter, Switch, Route, NavLink } from "react-router-dom";
+// import "./App.css";
+
+
+
+// export default withRouter(App);
