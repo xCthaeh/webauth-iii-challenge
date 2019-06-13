f3a874c7255f2dd65cd3b4b0be1be79eef88035d
Alyssa Jane <xcthaeh@gmail.com>
2019-06-13 01:09:36 -0400

    starting on app.js

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,10 +1,25 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 // import axios from "axios";
-// import Register from "./components/register";
+import Register from "./components/register";
 // import Login from "./components/login";
-// import { withRouter, Switch, Route, NavLink } from "react-router-dom";
-// import "./App.css";
+import { withRouter, Switch, Route, NavLink } from "react-router-dom";
+import "./App.css";
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
 
+        </header>
+        <section>
+          <Switch>
+            <Route path="/register" component={Register} />
+          </Switch>
+        </section>
+      </div>
+    );
+  }
+}
 
-// export default withRouter(App);
+export default withRouter(App);
